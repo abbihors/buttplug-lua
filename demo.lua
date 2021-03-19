f57e87fdaa669e498e5eddbde059c601b49d7ebe
abbiwyn <abbiwyn@gmail.com>
2021-03-19 17:25:36 +0200

    refactoring

--- demo.lua
+++ demo.lua
@@ -20,9 +20,28 @@ else
     end
 end
 
+local scanning = false
+
+-- Get a device from the Buttplug server
+function get_device()
+    -- Not connected yet
+    if not buttplug.got_server_info then
+        return
+    end
+
+    -- Try the device list first
+    if not buttplug.got_device_list then
+        buttplug.request_device_list()
+    elseif not scanning then
+        -- If device list was empty, start scanning
+        buttplug.start_scanning()
+        scanning = true
+    end
+end
+
 -- "Simulated" game loop
 function main_loop()
-    buttplug.init("Demo", "ws://127.0.0.1:12345")
+    buttplug.connect("example-app", "ws://127.0.0.1:12345")
 
     -- Each "tick" of your script
     while true do
@@ -31,14 +50,19 @@ function main_loop()
             print("error: couldn't connect to server")
             return
         end
-        
 
-        if not buttplug.has_devices() then
-            buttplug.get_devices()
+        if scanning and buttplug.has_devices() then
+            buttplug.stop_scanning()
+            scanning = false
+        elseif not buttplug.has_devices() then    
+            get_device()
         end
 
         -- Game doing other things, including running the thing
-        buttplug.send_vibrate_cmd(0, {0.2, 0.2})
+        if buttplug.has_devices() then
+            buttplug.send_vibrate_cmd(0, { 0.2 })
+        end
+        
         sleep(500)
     end
 end
